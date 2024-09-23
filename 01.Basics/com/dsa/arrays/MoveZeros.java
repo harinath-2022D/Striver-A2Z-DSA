dabd3e99b13f8b4eead36bc9af08208f2a91feda
Harinath Akkati <harireddyakkati.91@gmail.com>
2024-09-23 10:48:50 +0530

    move zeros

---
+++ 01.Basics/com/dsa/arrays/MoveZeros.java
@@ -0,0 +1,37 @@
+package com.dsa.arrays;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class MoveZeros {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 0, 0 };
+		moveZeroes(nums);
+		
+		Arrays.stream(nums).forEach(System.out::println);
+	}
+
+	public static void moveZeroes(int[] nums) {
+		int n = nums.length;
+		int[] copy = new int[n];
+
+		int i = 0;
+		int j = 0;
+
+		while (i < n) {
+			if (nums[i] != 0) {
+				copy[j++] = nums[i];
+			}
+			i++;
+		}
+
+		i = 0;
+
+		while (i < n) {
+			nums[i] = copy[i];
+			i++;
+		}
+	}
+
+}
